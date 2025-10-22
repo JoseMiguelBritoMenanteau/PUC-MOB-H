62e994d94f98f6cf651103c36a13153a4dd718bf
Gaspar Berner <gasparberner@gmail.com>
2025-10-22 20:19:03 -0300

    avance pagina "publish"

---
+++ SSApp/pages/publish/Publish_styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: { padding: 20},
+    titulo: { fontsize: 22, fontWeight: "bold", marginBottom: 20},
+    picker: { backgroundColor: "lightgray", marginVertical: 10},
+    label: { fontWeight: "bold", marginTop: 10},
+    input: { borderWidth: 1, borderRadius: 5, padding: 8, marginTop: 5, borderColor: "gray"},
+    campoContainer: { marginBottom: 10},
+    imagen: { width: 200, alignSelf: "center", height: 200, marginVertical: 10},
+    botonImagen: { padding: 10, borderRadius: 5, backgroundColor: "royalblue", marginVertical: 10},
+    botonImagenTexto: { textAlign: "center", color: "white"}
+});
