dc72a49470cda0202c7ae2ae74ecfd3efe8368fd
JoseMiguelBritoMenanteau <jose.brito@estudiante.uc.cl>
2025-10-22 22:33:18 -0300

    Avance en context

---
+++ SSApp/context/Context.tsx
@@ -0,0 +1,32 @@
+import { useContext, createContext, useState, createElement, ReactNode } from "react";
+
+interface ContextType {
+    username: string;
+    setUsername: (valor: string) => void
+}
+
+
+const AppContext = createContext<ContextType | undefined>(undefined);
+
+interface ProviderProps {
+    children: ReactNode;
+}
+
+export const AppProvider: React.FC<ProviderProps> = ({children}) => {
+    const [username, setUsername] = useState<string>('');
+
+    const value = { username, setUsername };
+
+    return <AppContext.Provider value = {value}>{children}</AppContext.Provider>;
+
+};
+
+export const useAppContext = () => {
+    
+}
+
+
+
+
+
+
