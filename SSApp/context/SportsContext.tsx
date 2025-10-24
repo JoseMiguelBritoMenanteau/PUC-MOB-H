a8fd478badce35fdb4a7bf10172d4deefe1bc50f
JoseMiguelBritoMenanteau <jose.brito@estudiante.uc.cl>
2025-10-24 16:28:41 -0300

    ae

--- SSApp/context/SportsContext.tsx
+++ SSApp/context/SportsContext.tsx
@@ -13,29 +13,6 @@ interface Sport {
     username: string;
 }
 
-interface SportsContextType {
-    sports: Sport[];
-    addSport: (sportData: Omit<Sport, 'id' | 'username'>, username: string) => void
-}
-
-const SportContext = createContext<SportsContextType | undefined>(undefined);
-
-interface SportsProviderProps {
-    children: ReactNode;
-}
-
-export const SportsProvider: React.FC<SportsProviderProps> = ({children}) => {
-    const [sports, setSports] = useState<Sport[]>([]);
-
-    const nextSportId = useRef(1);
-
-    const addSport = useCallback((sportData: Omit<Sport, 'id' | 'username'>, username: string)) => {
-        const  newId = nextActivityId.current
-    }
-    
-
-    return;
-}
 
 
 
