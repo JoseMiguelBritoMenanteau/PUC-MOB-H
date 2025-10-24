0d2c6bace1f2707ae2015698b3bb8deab37f7e37
Gaspar Berner <gasparberner@gmail.com>
2025-10-24 17:20:48 -0300

    listooo

--- SSApp/pages/publish/Publish.tsx
+++ SSApp/pages/publish/Publish.tsx
@@ -91,7 +91,7 @@ export default function Publish() {
       <KeyboardAvoidingView
         style={{ flex: 1 }}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={Platform.OS === "ios" ? 135 : 135}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0}
       >
         <ScrollView contentContainerStyle={styles.container}>
           <Text style={styles.titulo}>Publicar actividad</Text>
